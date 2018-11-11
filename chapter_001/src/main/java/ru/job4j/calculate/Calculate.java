1e3854dbf7135e763c18254f67ae2f0e3a0de037
Rodriguez111 <dreamhunter1@ukr.net>
2018-11-12 01:12:34 +0200

    1.4. JUnit[#84598]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -19,5 +19,15 @@ public class Calculate {
 	}
 	
 	
+	/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+		public String echo(String name) {
+			return "Echo, echo, echo : " + name;
+		}
+	
+	
 	
 }
