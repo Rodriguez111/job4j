818875117059f4218a894e756efb7303a27a1bb1
Rodriguez111 <dreamhunter1@ukr.net>
2018-11-12 00:43:51 +0200

    1.3. JavaDoc. Code convention[#84597]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+*Calculate.
+*
+*@author Rodriguez (mymail4java@gmail.com)
+*prints to console "Hello World!"
+*
+Main.
+@param args - args
+*/
+
 
 public class Calculate {
 	public static void main(String[] args){
