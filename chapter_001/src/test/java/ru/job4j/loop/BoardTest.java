653ad83350f7be713bf3e25454d00c978727a02f
Rodriguez111 <dreamhunter1@ukr.net>
2018-11-18 21:47:48 +0200

    5.3. Построить шахматную доску в псевдографике.[#84572]

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -14,4 +14,12 @@ public class BoardTest {
        String ln = System.lineSeparator();
        assertThat(result, is(String.format("X X%s X %sX X%s", ln, ln, ln)));
     }
+
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        String ln = System.lineSeparator();
+        assertThat(result, is(String.format("X X X%s X X %sX X X%s X X %s", ln, ln, ln, ln)));
+    }
 }
