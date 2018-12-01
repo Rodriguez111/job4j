c064ec0dd99dd2834ac49dd78f9ee2b37cac05d4
Rodriguez111 <dreamhunter1@ukr.net>
2018-12-01 04:44:13 +0200

    2. Реализовать класс Tracker[#84652]

--- chapter_002/src/main/java/tracker/Tracker.java
+++ chapter_002/src/main/java/tracker/Tracker.java
@@ -1,42 +1,111 @@
 package tracker;
 
+import java.util.Arrays;
 import java.util.Random;
+
+/**
+ * Accounting claims.
+ */
 
 public class Tracker {
     private int position = 0;
     private static final Random RANDOM = new Random();
     private Item[] items = new Item[100];
 
+    /**
+     *
+     * @param item - item to add.
+     * @return - added item.
+     */
     public Item add(Item item) {
         item.setId(generateID());
         items[position++] = item;
         return item;
     }
 
+    /**
+     *
+     * @param id - id of the item we want to replace.
+     * @param item - new item.
+     */
     public void replace(String id, Item item) {
-
+        for (int i = 0; i <= items.length; i++) {
+            if (items[i] == null) {
+                break;
+            }
+            if (items[i].getId().equals(id)) {
+                items[i] = item;
+            }
+        }
     }
 
+    /**
+     *
+     * @param id - id of the item we want to delete.
+     */
     public void delete(String id) {
-
+        for (int i = 0; i <= items.length; i++) {
+            if (items[i] == null) {
+                break;
+            }
+            if (items[i].getId().equals(id)) {
+             System.arraycopy(items, i + 1, items, i, items.length - (i + 1));
+             items[items.length - 1] = null;
+             position--;
+            }
+        }
     }
 
+    /**
+     *
+     * @return - all not-null items from the array.
+     */
     public Item[] getAll() {
     Item[] allItems = new Item[this.position];
-    for (int i = 0; i <= this.position; i++){
+    for (int i = 0; i < this.position; i++) {
         allItems[i] =  items[i];
     }
     return allItems;
     }
 
+    /**
+     *
+     * @param key - name we are looking for.
+     * @return - array weth found items.
+     */
     public Item[] findByName(String key) {
-
+        Item[] foundNames = new Item[this.position];
+        int count = 0;
+        for (int i = 0; i < this.position; i++) {
+            if (items[i].getName().equals(key)) {
+                foundNames[count]  = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(foundNames, count);
     }
 
+    /**
+     *
+     * @param id - id of the item we want to find.
+     * @return - found item.
+     */
     public Item findById(String id) {
-
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                break;
+            }
+            if (items[i].getId().equals(id)) {
+             return  items[i];
+            }
+        }
+        return null;
     }
 
+    /**
+     *
+     * @return - generated id.
+     */
     private String generateID() {
         return String.valueOf(System.currentTimeMillis() + RANDOM.nextInt(100));
     }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.tracker;
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import tracker.Item;
+import tracker.Tracker;
+
+public class TrackerTest {
+
+    @Test
+    public void whenAddItemReturnsSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Test1");
+        assertThat(tracker.add(item), is(item));
+    }
+
+    @Test
+    public void whenReplaceNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Test1");
+        tracker.add(item);
+        Item item2 = new Item("Test2");
+        item2.setId(item.getId());
+        tracker.replace(item.getId(), item2);
+        String actual = tracker.findById(item2.getId()).getName();
+        assertThat(actual, is("Test2"));
+    }
+
+    @Test
+    public void whenDeleteOneOfThreeRemainsTwo() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test1");
+        Item item2 = new Item("Test2");
+        Item item3 = new Item("Test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = new Item[2];
+        expected[0] = item1;
+        expected[1] = item3;
+        tracker.delete(item2.getId());
+        Item[] actual = tracker.getAll();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenGetAllThenReturnsThree() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test1");
+        Item item2 = new Item("Test2");
+        Item item3 = new Item("Test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = new Item[3];
+        expected[0] = item1;
+        expected[1] = item2;
+        expected[2] = item3;
+        Item[] actual = tracker.getAll();
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenSearchNameTest2ThenFindNameTest2() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test1");
+        Item item2 = new Item("Test2");
+        Item item3 = new Item("Test2");
+        Item item4 = new Item("Test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item[] expected = new Item[2];
+        expected[0] = item2;
+        expected[1] = item3;
+        Item[] actual = tracker.findByName("Test2");
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenSearchByIDFindItemWithThisID() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Test1");
+        Item item2 = new Item("Test2");
+        Item item3 = new Item("Test3");
+        Item item4 = new Item("Test4");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        String testID = item3.getId();
+        String expected = "Test3";
+        String actual = tracker.findById(testID).getName();
+        assertThat(actual, is(expected));
+    }
+
+
+}
