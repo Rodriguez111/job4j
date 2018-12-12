52562ba0c1027bb73adedbe5c0d4793a594951d1
Rodriguez111 <dreamhunter1@ukr.net>
2018-12-12 16:11:11 +0200

    3. Конвертация двумерного массива в ArrayList[#84622]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,23 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ConvertMatrix2List {
+
+    /**
+     *
+     * @param array - array which we convert to list.
+     * @return - result list.
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] eachRow : array) {
+            for (int eachColumn : eachRow) {
+                list.add(eachColumn);
+            }
+        }
+        return list;
+    }
+}
