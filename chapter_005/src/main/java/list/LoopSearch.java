ebe06a570d3bf71da67828c35d8cb9b76849c16a
ruben.khodzhaev <rkhodzhaev@yahoo.com>
2019-01-31 22:14:24 +0200

    5.3.4. Задан связанный список. Определить цикличность.[#107822] Tortoise and hare algorithm

--- chapter_005/src/main/java/list/LoopSearch.java
+++ chapter_005/src/main/java/list/LoopSearch.java
@@ -1,16 +1,46 @@
+/**
+ * Задан связанный список.
+ *
+ * class Node<T> {
+ *    T value;
+ *    Node<T> next;
+ * }
+ *
+ * Node first = new Node(1);
+ * Node two = new Node(2);
+ * Node third = new Node(3);
+ * Node four = new Node(4);
+ *
+ * first.next = two;
+ * two.next = third;
+ * third.next = four;
+ * four.next = first;
+ *
+ * Написать алгоритм определяющий, что список содержит замыкания.
+ *
+ * boolean hasCycle(Node first);
+ *
+ * Обратите внимание, что список может быть замкнут и в середине. К примеру, 3-й узел будет ссылаться на 2-й узел.
+ * Определение зацикленности необходимо реализовать путем прохода по узлам, без использования коллекций.
+ *
+ * Решение с пом. алгоритма черепахи и зайца
+ */
 package list;
 
 public class LoopSearch<T> {
    static boolean hasCycle(Node first) {
+       Node slow = first;
+       Node fast = first;
        boolean result = false;
-       Node current = first;
-       while (current.next != null) {
-           if (current.label) {
-               result = true;
-               break;
+       if (slow.next != null && fast.next.next != null) {
+           while (fast.next != null && fast.next.next != null) {
+               slow =  slow.next;
+               fast = fast.next.next;
+               if (slow.equals(fast)) {
+                   result = true;
+                   break;
+               }
            }
-           current.label = true;
-           current =  current.next;
        }
             return result;
     }
@@ -20,7 +50,7 @@ public class LoopSearch<T> {
             this.value = value;
         }
         T value;
-        boolean label = false;
         Node<T> next;
     }
+
 }
