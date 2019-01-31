3ee19e5d598c2b389ac052bb8bee9cd37500a6d4
ruben.khodzhaev <rkhodzhaev@yahoo.com>
2019-01-31 04:50:40 +0200

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#107818] Refactor using composition.

--- chapter_005/src/main/java/list/SimpleLinkedList.java
+++ chapter_005/src/main/java/list/SimpleLinkedList.java
@@ -1,7 +1,6 @@
 package list;
 
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
+import java.util.*;
 
 public class SimpleLinkedList<E> implements Iterable<E> {
     private int size;
@@ -9,7 +8,10 @@ public class SimpleLinkedList<E> implements Iterable<E> {
     private Node<E> first;
     private Node<E> last;
 
-
+    /**
+     * Add element to the end of the list
+     * @param item - item to add
+     */
     public void add(E item) {
         final Node<E> currentLast = last;
         Node<E> newItem = new Node<>(currentLast, null, item);
@@ -23,7 +25,13 @@ public class SimpleLinkedList<E> implements Iterable<E> {
         modCount++;
     }
 
+    /**
+     * Get element by the index.
+     * @param index - index of the element.
+     * @return element by the index.
+     */
     public E get(int index) {
+        checkIndex(index);
         Node<E> result = first;
         for (int i = 0; i < index; i++) {
             result = result.next;
@@ -31,6 +39,102 @@ public class SimpleLinkedList<E> implements Iterable<E> {
         return result.data;
     }
 
+    /**
+     * Delete element by the index.
+     * @param index - index of the element.
+     * @return result of the operation.
+     */
+    public boolean delete(int index) {
+        checkIndex(index);
+        Node<E> toDelete = first;
+        for (int i = 0; i < index; i++) {
+            toDelete = toDelete.next;
+        }
+        if (size == 1) {
+            deleteSingle();
+        } else if (toDelete.previous == null) {
+            first = toDelete.next;
+            first.previous = null;
+        } else if (toDelete.next == null) {
+            last = toDelete.previous;
+            toDelete.next = null;
+        } else {
+            toDelete.previous.next = toDelete.next;
+            toDelete.next.previous = toDelete.previous;
+        }
+        size--;
+        modCount--;
+        return true;
+    }
+
+    /**
+     * Delete last standing element.
+     */
+    public void deleteSingle() {
+        first = null;
+        last = null;
+        size--;
+        modCount--;
+    }
+
+
+    /**
+     * Add element by the index.
+     * @param index - index of the element.
+     * @param item - item to add.
+     * @return result of the operation.
+     */
+    public boolean add(int index, E item) {
+        if (index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            add(item);
+        } else {
+            Node<E> currentElement = first;
+            for (int i = 0; i < index; i++) {
+                currentElement = currentElement.next;
+            }
+            Node<E> newElement = new Node<>(currentElement.previous, currentElement, item);
+            currentElement.previous = newElement;
+            newElement.previous.next = newElement;
+            size++;
+            modCount++;
+        }
+            return true;
+    }
+
+    /**
+     * Add element to the first position (index = 0).
+     * @param item  - item to add.
+     * @return result of the operation.
+     */
+    public boolean addFirst(E item) {
+        Node<E> currentFirst = first;
+        Node<E> newItem = new Node<>(null, currentFirst, item);
+        first = newItem;
+        if (currentFirst == null) {
+            last = newItem;
+        } else {
+            currentFirst.previous = newItem;
+        }
+        modCount++;
+        size++;
+        return true;
+    }
+
+
+    /**
+     * Checks if index is not out of bound of the list.
+     * @param index - index to check.
+     */
+    private void checkIndex(int index) {
+        if (first == null ||  size < index) {
+            throw new NoSuchElementException();
+        }
+    }
 
     @Override
     public Iterator<E> iterator() {
@@ -62,6 +166,10 @@ public class SimpleLinkedList<E> implements Iterable<E> {
             this.next = next;
             this.data = data;
         }
+    }
+
+    public int getSize() {
+        return size;
     }
 
 }
--- chapter_005/src/main/java/list/SimpleStack.java
+++ chapter_005/src/main/java/list/SimpleStack.java
@@ -1,60 +1,23 @@
 package list;
 
 public class SimpleStack<E> {
-    private int size;
-    private int modCount;
-    private Node<E> first;
-    private Node<E> last;
+  private SimpleLinkedList<E> simpleLinkedList = new SimpleLinkedList<>();
 
     public void push(E data) {
-        Node<E> currentFirst = first;
-        Node<E> newItem = new Node<>(data, null, currentFirst);
-        first = newItem;
-        if (currentFirst == null) {
-            last = newItem;
-        } else {
-            currentFirst.previous = newItem;
-        }
-        modCount++;
-        size++;
+        simpleLinkedList.addFirst(data);
     }
 
     public E poll() {
-        E result = null;
-        if (first != null) {
-            Node<E> resultNode = first;
-            result  = resultNode.data;
-            if (resultNode.next != null) {
-                first = resultNode.next;
-                first.previous = null;
-            } else {
-                first = null;
-                last = null;
-            }
-            modCount++;
-            size--;
-        }
-        return  result;
-    }
-
-    public E peek() {
-        E result = null;
-        if (first != null) {
-            result = first.data;
-        }
+     E result = null;
+     if (simpleLinkedList.getSize() > 0) {
+         result = simpleLinkedList.get(0);
+         simpleLinkedList.delete(0);
+     }
         return result;
     }
 
-    private static class Node<E> {
-        private E data;
-        private Node<E> previous;
-        private Node<E> next;
-
-        public Node(E data, Node<E> previous, Node<E> next) {
-            this.data = data;
-            this.previous = previous;
-            this.next = next;
-        }
+    public E peek() {
+      return simpleLinkedList.getSize() > 0 ? simpleLinkedList.get(0) : null;
     }
 
 
