388c235963df4d0da6a30ae28fa4eac617522e35
ruben.khodzhaev <rkhodzhaev@yahoo.com>
2019-02-01 11:20:11 +0200

    2. Не перекрывать equals hashCode[#107841]

--- chapter_005/src/test/java/map/User.java
+++ chapter_005/src/test/java/map/User.java
@@ -1,6 +1,9 @@
 package map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
    private String name;
@@ -11,5 +14,17 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 2, new GregorianCalendar(1980,0,31));
+        User user2 = new User("Ivan", 2, new GregorianCalendar(1980,0,31));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        System.out.println(map);
     }
 }
