f6ea6a3d102c8c3f6698bfa66fd87258d5484100
ruben.khodzhaev <rkhodzhaev@yahoo.com>
2019-02-01 12:03:23 +0200

    4. Переопределить только equals[#107840]

--- chapter_005/src/test/java/map/User.java
+++ chapter_005/src/test/java/map/User.java
@@ -13,11 +13,17 @@ public class User {
         this.birthday = birthday;
     }
 
-
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
     }
+
+
 
     public static void main(String[] args) {
         User user1 = new User("Ivan", 2, new GregorianCalendar(1980,0,31));
