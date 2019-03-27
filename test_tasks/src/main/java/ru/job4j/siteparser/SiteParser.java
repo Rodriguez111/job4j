7a3a7f97fc9fb0f95178c299d845c89292e68d07
ruben.khodzhaev <rkhodzhaev@yahoo.com>
2019-03-27 22:36:06 +0200

    Last changes

---
+++ test_tasks/src/main/java/ru/job4j/siteparser/SiteParser.java
@@ -0,0 +1,19 @@
+package ru.job4j.siteparser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class SiteParser {
+    private static final String URL = "https://www.sql.ru";
+    private Document document;
+
+    private void connect() {
+        try {
+            this.document = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
